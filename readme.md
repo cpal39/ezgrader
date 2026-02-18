a89d458db3cf222f46a593407a6d2f0be6382791
Chris Paldino <cpaldino9@gmail.com>
2026-02-17 21:13:37 -0500

    Remove bootstrap; add readme

---
+++ readme.md
@@ -0,0 +1,11 @@
+# EZgrader
+
+A simple tool to see what grade should be given on a test where all questions are worth the same amount of points.
+
+## Why this exists
+
+My mother is a teacher and wanted an easy grader she could quickly access. Love you Mom!
+
+## Usage
+
+Enter the number of questions that are on the test, and the grading breakdown will be displayed automatically as the input changes.
--- script.js
+++ script.js
@@ -1,27 +1,22 @@
 $(document).ready(function () {
-	$("#navLink").click(function () {
-		location.reload(true);
-	});
-	$("form").submit(function (e) {
-		e.preventDefault();
-		$("#results").empty();
-		let numQ = $("input[name='numQ']").val();
-		$("#results").append(
-			"<div id='resultsRow' class='row bg-primary'><div class='col-6'>Number Wrong</div><div class='col-6'>Percent</div></div>"
-		);
-		for (let i = 0; i <= numQ; i++) {
-			let numRight = numQ - i;
-			let percent = Math.round(numRight / numQ * 100);
-			if (i % 2 == 0) {
-				$("#results").append(
-					"<div class='row bg-secondary'><div class='col-6'>" + i + "</div><div class='col-6'>" + percent + "%</div></div>"
-				);
-			}
-			else {
-				$("#results").append(
-					"<div class='row bg-primary'><div class='col-6'>" + i + "</div><div class='col-6'>" + percent + "%</div></div>"
-				);
-			}
-		}
-	});
+  $("#navLink").click(function () {
+    location.reload(true);
+  });
+  $("#numQInput").on("input", () => {
+    let numQ = Number.parseInt($("input[name='numQ']").val());
+    if (!numQ) {
+      $("#results").empty();
+      return;
+    }
+    let resultsStr =
+      "<div id='resultsHeader' class='row bg-primary'><div>Number Wrong</div><div>Percent</div></div><div id='resultsRows'>";
+    for (let i = 0; i <= numQ; i++) {
+      let numRight = numQ - i;
+      let percent = Math.round((numRight / numQ) * 100);
+      resultsStr += `<div class='row bg-${i % 2 == 0 ? "secondary" : "primary"}'><div>${i}</div><div>${percent}%</div></div>`;
+    }
+    resultsStr += "</div></div>";
+    $("#results").empty();
+    $("#results").append(resultsStr);
+  });
 });
